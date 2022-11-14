9bf763fc706a88fdd7bd53f0fa62082c390c364a
Yuri Oku <yuri.oku.astro@gmail.com>
2022-11-14 22:32:04 +0900

    added bouwens15 SFRF

--- README.md
+++ README.md
@@ -17,14 +17,13 @@ import load_obsdata as obs
 d = obs.load_obsdata("SMF", 5, 6)
 
 plt.figure(facecolor="white")
-for i in range(len(d)):
-  di = d[i]
+for di in d:
   plt.plot(di["x"], di["y"], label=di["label"], ms=3)
   plt.fill_between(di["x"], di["y1"], di["y2"], alpha=0.3)
 plt.xlabel(r"log M$_*$ [M$_\odot$]")
 plt.ylabel(r"log $\Phi$ [dex$^{-1}$ Mpc$^{-3}$]")
 plt.legend()
-plt.savefig("example.png")
+plt.savefig("example.png", bbox_inches="tight")
 plt.close()
 ```
 <img src="./example.png" width=500px>
--- format_data.py
+++ format_data.py
@@ -2,14 +2,15 @@
 import numpy as np
 import math
 
-file = "data/SHMR/stefanon2021_z10.csv"
+file = "data/SFRF/bouwens2015_z5.csv"
 
 d = np.loadtxt(file)
 
 for line in d:
-  c1 = line[0]
-  c2 = math.log10(line[1]*1e-3)
-  c3 = c2 - math.log10((line[1]-line[2])*1e-3)
-  c4 = math.log10((line[1]+line[3])*1e-3) - c2
+  c1 = math.log10(line[0])
+  c2 = math.log10(line[1]*1e-2)
+  c3 = c2 - math.log10((line[1] - line[2])*1e-2)
+  # c3 = 0
+  c4 = math.log10((line[1]+line[2])*1e-2) - c2
   print("{:.3f}, {:.3f}, {:.3f}, {:.3f}".format(c1, c2, c3, c4))
 # %%
