c930a434736bc617b30935057a17e9f1874b9e94
Yuri Oku <yuri.oku.astro@gmail.com>
2022-12-13 22:19:09 +0900

    added Behroozi SHMR

--- format_data.py
+++ format_data.py
@@ -2,17 +2,15 @@
 import numpy as np
 import math
 
-file = "data/SFRF/robotham2011_z0.05.csv"
+file = "data/SHMR/behroozi2019_z10.csv"
 
 d = np.loadtxt(file)
 
 for line in d:
-  c1 = math.log10(line[0])
-  c2 = math.log10(line[1] * 1e-2)
-  try:
-    c3 = c2 - math.log10((line[1] - line[2]) * 1e-2)
-  except:
-    c3 = -10000
-  c4 = math.log10((line[1] + line[2]) * 1e-2) - c2
-  print("{:.3f}, {:.3f}, {:.3f}, {:.3f}".format(c1, c2, c3, c4))
+  if line[15] != 0:
+    c1 = line[0]
+    c2 = line[4]
+    c3 = line[6]
+    c4 = line[5]
+    print("{:.3f}, {:.3f}, {:.3f}, {:.3f}".format(c1, c2, c3, c4))
 # %%
