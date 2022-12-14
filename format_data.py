a05ba652ee80596e1f7e4042e7ab8fea72465836
Yuri Oku <yuri.oku.astro@gmail.com>
2022-12-14 23:30:17 +0900

    add metal abundance ratio

--- format_data.py
+++ format_data.py
@@ -2,23 +2,22 @@
 import numpy as np
 import math
 
-file = "data/SFMS/salmon2015_z6.csv"
+file = "data/MZR/N_O/andrews2013_z0.1.csv"
 
 d = np.loadtxt(file)
 
-ncol = len(d[0])
+# ncol = len(d[0])
+# for i in range(ncol):
+#   c1 = d[0][i]
+#   c2 = d[1][i]
+#   c3 = d[2][i]
+#   c4 = d[2][i]
+#   print("{:.3f}, {:.3f}, {:.3f}, {:.3f}".format(c1, c2, c3, c4))
 
-for i in range(ncol):
-  c1 = d[0][i]
-  c2 = d[1][i]
-  c3 = d[2][i]
-  c4 = d[2][i]
-  print("{:.3f}, {:.3f}, {:.3f}, {:.3f}".format(c1, c2, c3, c4))
-
-# for line in d:
-#     c1 = line[0]
-#     c2 = math.log10(line[16])
-#     c3 = c2 - math.log10(line[16] - line[18])
-#     c4 = math.log10(line[16] + line[17]) - c2
-#     print("{:.3f}, {:.3f}, {:.3f}, {:.3f}".format(c1, c2, c3, c4))
+for line in d:
+    c1 = (line[0] + line[1]) / 2
+    c2 = line[2]
+    c3 = line[3]
+    c4 = line[3]
+    print("{:.3f}, {:.3f}, {:.3f}, {:.3f}".format(c1, c2, c3, c4))
 # %%
--- load_obsdata.py
+++ load_obsdata.py
@@ -2,7 +2,31 @@ import numpy as np
 import subprocess
 import os
 
-def load_obsdata(key, z1, z2, IMF="Chabrier", verbose=False):
+def convert_imf(imf_input, imf_output):
+    f_convert = 1
+    # convert input imf to salpeter
+    if imf_input == "Salpeter":
+        pass
+    elif imf_input == "Chabrier":
+        f_convert = 1.7
+    elif imf_input == "Kroupa":
+        f_convert = 2.0 # for Salpeter IMF with a cut-off at 0.1 Msun (Kauffmann+ 2003)
+    else:
+        raise Exception("imf_input does not match")
+    
+    #convert from salpeter to output imf
+    if imf_output == "Salpeter":
+        pass
+    elif imf_output == "Chabrier":
+        f_convert /= 1.7
+    elif imf_output == "Kroupa":
+        f_convert /= 2.0
+    else:
+        raise Exception("imf_output does not match")
+    
+    return f_convert
+
+def load_obsdata(key, z1, z2, IMF="Chabrier", ratio="undef", verbose=False):
     """
     returns observational data points
 
@@ -16,6 +40,10 @@ def load_obsdata(key, z1, z2, IMF="Chabrier", verbose=False):
       maximum redshift of data to load
     IMF: string
       correct stellar mass for the IMF
+    ratio: string
+      necessary when key is MZR. select the abundance ratio to output
+    verbose: bool
+      output details when true
 
     Returns
     -------
@@ -33,6 +61,8 @@ def load_obsdata(key, z1, z2, IMF="Chabrier", verbose=False):
     Examples
     --------
     >>> load_obsdata("SMF", 0, 2)
+    
+    >>> load_obsdata("MZR", 2, 3, IMF="Kroupa", ratio="O/H")
     """
 
     def pprint(txt):
@@ -41,16 +71,34 @@ def load_obsdata(key, z1, z2, IMF="Chabrier", verbose=False):
         else:
             pass
 
-    if IMF != "Chabrier" and IMF != "Salpeter":
-        raise ValueError("Choose IMF from Chabrier or Salpeter")
+    # sanity check of input parameters
+    if IMF != "Chabrier" and IMF != "Salpeter" and IMF != "Kroupa":
+        raise ValueError("Choose IMF from Chabrier, Salpeter, or Kroupa")
+    
+    if key == "MZR":
+        if ratio == "undef":
+            raise ValueError("Select which abundance ratio to output")
 
-    out = []
+    # check if data directory is accessible
     rootdir = os.getenv("OBSDATA_DIR")
     if rootdir == None:
         raise Exception("ERROR: environment variable OBSDATA_DIR is not set")
-    cmd = "ls -v1 "+rootdir+"/data/"+key+"/*.csv"
+    
+    # get file names
+    if key != "MZR":
+        cmd = "ls -v1 "+rootdir+"/data/"+key+"/*.csv"
+    else:
+        if ratio == "O/H":
+            cmd = "ls -v1 "+rootdir+"/data/"+key+"/O_H/*.csv"
+        elif ratio == "N/O":
+            cmd = "ls -v1 "+rootdir+"/data/"+key+"/N_O/*.csv"
+        else:
+            raise ValueError("Available data of abundance ratio are O/H or N/O")
+    print(cmd)
     cp = subprocess.run(cmd, capture_output=True, text=True, shell=True)
     fname = cp.stdout.split("\n")
+    out = []
+    print(fname) #debug
     for f in fname:
         if f == "":
             continue
@@ -108,32 +156,21 @@ def load_obsdata(key, z1, z2, IMF="Chabrier", verbose=False):
             continue
 
         # IMF correction
-        if data_imf != "Chabrier" and data_imf != "Salpeter":
+        if data_imf != "Chabrier" and data_imf != "Salpeter" and data_imf != "Kroupa":
             raise Exception("IMF not found in the header")
 
-        convert_chabrier_to_salpeter = np.log10(1.7)
+        log_conversion_factor = np.log10(convert_imf(data_imf, IMF))
         if key == "SMF":
-            if IMF == "Salpeter" and data_imf == "Chabrier":
-                x += convert_chabrier_to_salpeter
-            elif IMF == "Chabrier" and data_imf == "Salpeter":
-                x -= convert_chabrier_to_salpeter
+            x += log_conversion_factor
         elif key == "SHMR":
-            if IMF == "Salpeter" and data_imf == "Chabrier":
-                y += convert_chabrier_to_salpeter
-            elif IMF == "Chabrier" and data_imf == "Salpeter":
-                y -= convert_chabrier_to_salpeter
+            y += log_conversion_factor
         elif key == "SFRF":
-            if IMF == "Salpeter" and data_imf == "Chabrier":
-                x += convert_chabrier_to_salpeter
-            elif IMF == "Chabrier" and data_imf == "Salpeter":
-                x -= convert_chabrier_to_salpeter
+            x += log_conversion_factor # same conversion factor for SFR?
         elif key == "SFMS":
-            if IMF == "Salpeter" and data_imf == "Chabrier":
-                x += convert_chabrier_to_salpeter
-                y += convert_chabrier_to_salpeter
-            elif IMF == "Chabrier" and data_imf == "Salpeter":
-                x -= convert_chabrier_to_salpeter
-                y -= convert_chabrier_to_salpeter
+            x += log_conversion_factor
+            y += log_conversion_factor # ?
+        elif key == "MZR":
+            x += log_conversion_factor
         
 
         pprint("Loading data of "+author.replace("+",
